b294df37ebd5797e93af590648609a644c74696c
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-03-16 01:59:28 -0500

    deploying

--- server.js
+++ server.js
@@ -15,9 +15,14 @@ app.use(express.static("public"))
 app.use(require("./routes/apiRoutes"))
 app.use(require("./routes/htmlRoutes"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
 
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`)
-  });
+  console.log(`App running on port ${PORT}!`)
+});
