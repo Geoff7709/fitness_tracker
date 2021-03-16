cd7e5772281cc51d8495704775ab74925d11b50b
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-03-16 12:51:39 -0500

    fixed validation for database

--- models/Workout.js
+++ models/Workout.js
@@ -9,7 +9,6 @@ const WorkoutSchema = new Schema({
         {
             type: {
                 type: String,
-                // required: true,
             },
             name: {
                 type: String,
@@ -18,39 +17,23 @@ const WorkoutSchema = new Schema({
             duration: {
                 type: Number,
                 trim: true,
-                // required: true,
             },
             weight: {
                 type: Number,
                 trim: true,
-                // required: [
-                //     function() { return this.type === "resistance"; },
-                //     "Weight is required for resistance exercises."
-                //   ]
             },
             reps: {
                 type: Number,
                 trim: true,
-                // required: [
-                //     function() { return this.type === "resistance"; },
-                //     "Number of reps is required for resistance exercises."
-                //   ]
             },
             sets: {
                 type: Number,
                 trim: true,
-                // required: [
-                //     function() { return this.type === "resistance"; },
-                //     "Number of sets is required for resistance exercises."
-                //   ]
             },
             distance: {
                 type: Number,
                 trim: true,
-                // required: [
-                //     function() { return this.type === "cardio"; },
-                //     "Distance is required for cardio exercises, when not applicable enter 0."
-                //   ]
+                min: 0,
             },
         }
     ],
